8d1c1097ddefa41419ac7b38a31f6d86f1ec5997
RafudoGoulart <rafael.c.goulart2009@gmail.com>
2025-11-20 10:46:00 -0300

    melhorias layout

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,8 @@
-// menu hamburguer
+// Menu responsivo mobile
 const mobileMenu = document.getElementById('mobile-menu');
 const navLinks = document.getElementById('nav-links');
 
 mobileMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Adiciona ou remove a classe 'active'
-    mobileMenu.classList.toggle('is-active'); // Alterna a classe do botão
+    navLinks.classList.toggle('active'); 
+    mobileMenu.classList.toggle('is-active');
 });
